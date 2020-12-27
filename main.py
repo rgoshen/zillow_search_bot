0d36f5ac7f2f7fcb0d084649912831aee7b18898
rgoshen <rick.goshen@gmail.com>
2020-12-26 20:56:35 -0700

    created spreadsheet from responses

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 
 zillow_url = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C" \
              "%22mapBounds%22%3A%7B%22west%22%3A-122.65170502835149%2C%22east%22%3A-122.21495297164851%2C%22south%22" \
@@ -15,7 +17,9 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 "
                   "Safari/537.36 "
 }
-
+CHROME_DRIVER_PATH = "/home/rgoshen/Development/chromedriver"
+google_form_url = "https://docs.google.com/forms/d/e/1FAIpQLScZdfWw0hBpE2jG69Io1HN8w57uGQuy78O8LN51hbevKFaXjg" \
+                  "/viewform?usp=sf_link "
 all_links = []
 all_prices = []
 all_addresses = []
@@ -49,4 +53,24 @@ for element in all_price_elements:
         all_prices.append(price)
 
 # list for all addresses
+all_address_elements = soup.select(".list-card-info address")
+all_addresses = [address.get_text().split(" | ")[-1] for address in all_address_elements]
 
+# Create Spreadsheet using Google Form
+driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+
+for n in range(len(all_links)):
+    driver.get(google_form_url)
+    time.sleep(2)
+    address = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div['
+                                           '1]/div/div[1]/input')
+    price = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div['
+                                         '1]/div/div[1]/input')
+    link = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div['
+                                        '1]/div/div[1]/input')
+    submit_button = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
