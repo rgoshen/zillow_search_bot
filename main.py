30f0ec2a035db54b8234d5380b0a17f1d0b13317
rgoshen <rick.goshen@gmail.com>
2020-12-26 19:22:10 -0700

    created zillow search

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+zillow_url = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C" \
+             "%22mapBounds%22%3A%7B%22west%22%3A-122.65170502835149%2C%22east%22%3A-122.21495297164851%2C%22south%22" \
+             "%3A37.65310711027443%2C%22north%22%3A37.89727419859719%7D%2C%22mapZoom%22%3A12%2C%22isMapVisible%22" \
+             "%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1" \
+             "%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B" \
+             "%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C" \
+             "%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value" \
+             "%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C" \
+             "%22isListVisible%22%3Atrue%7D"
+headers = {
+    "Accept-Language": "en-US,en;q=0.9",
+    "User-Agent": "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 "
+                  "Safari/537.36 "
+}
+
+# zillow search
+response = requests.get(url=zillow_url, headers=headers)
+return_webpage = response.text
+soup = BeautifulSoup(return_webpage, 'html.parser')
+
+# list of links returned
+
